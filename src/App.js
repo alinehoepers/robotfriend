4e0bc1f1d37c06b0aeb3605c67b9db27a2b2dff4
Aline Martins Hoepers <ath@certi.org.br>
2018-05-03 10:55:05 -0300

    Adds <scroll> to the searchbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 const state = {
    
 }
+
+
 class App extends Component {
     constructor (){
         super()
@@ -17,9 +20,8 @@ class App extends Component {
 
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response =>response.json())
-        .then(users => this.setState({robots:users}));
-       
+        .then(response => response.json())
+        .then(users => {this.setState({robots:users})});
     }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
@@ -33,7 +35,9 @@ class App extends Component {
             <div className = 'tc'>
             <h1 className = 'f1'> RoboFriends </h1>
             <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots= {filteredRobots}/>
+            <Scroll>
+            <CardList robots={filteredRobots} />
+            </Scroll>
             </div>
         );
     }
